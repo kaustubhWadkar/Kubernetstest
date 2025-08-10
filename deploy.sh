5158f05a1c79eecb0b4afbbbebff36a7dd0ab9ce
Kaustubh Wadkar <151868024+kaustubhWadkar@users.noreply.github.com>
2025-08-11 00:15:48 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ chmod 777 logs.txt
 aws eks --region us-east-1 update-kubeconfig --name my-eks-cluster2
 
 kubectl apply -f ./deployment.yaml >> logs.txt 2>&1
-kubectl rollout restart deployment ekslove-deployment -n hello-world >> logs.txt 2>&1
 kubectl apply -f ./service.yaml >> logs.txt 2>&1
 
 touch abc123.txt
