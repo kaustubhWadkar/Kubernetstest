f6a2f1443fd93cf9ac1a729390fea79849b809f8
Kaustubh Wadkar <151868024+kaustubhWadkar@users.noreply.github.com>
2025-08-10 23:20:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 cd /home/ubuntu/kubernets_cluster/tws-e-commerce-app_hackathon/Kubernetstest
 touch logs.txt
 chmod 777 logs.txt
+aws eks --region us-east-1 update-kubeconfig --name my-eks-cluster2
 kubectl apply -f ./deployment.yaml >> logs.txt 2>&1
 kubectl apply -f ./service.yaml >> logs.txt 2>&1
 touch abc123.txt
