d2f4d73f2c774cfcc489a7b293aae1618a442357
Kaustubh Wadkar <151868024+kaustubhWadkar@users.noreply.github.com>
2025-08-10 22:55:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+sudo su
 {
   cd /home/ubuntu/kubernets_cluster/tws-e-commerce-app_hackathon/Kubernetstest
   touch abc.txt
   kubectl apply -f ./deployment.yaml
   kubectl apply -f ./service.yaml
+  touch tested
 } >> ./logs 2>&1
